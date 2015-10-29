fdf676fc82794b69addcf82f90d0f6ac8d40c13a
Sorum Panchal <svpanchal@gmail.com>
2015-10-29 10:30:36 -0400

    Created ability to create a new concert

--- client/app/concerts/newconcert.controller.js
+++ client/app/concerts/newconcert.controller.js
@@ -1,1 +1,25 @@
-newconcert.controller.js
+'use strict';
+
+angular.module('indiesoundsApp')
+.controller('NewConcertCtrl', function ($scope, $stateParams, Auth, concertsServices) {
+
+    console.log('Im Alive and kicking');
+
+    Auth.getCurrentUser().$promise.then(function(currentUser) {
+        $scope.userid = currentUser._id;
+        console.log('$scope.userid is: ' + JSON.stringify($scope.userid));
+    });
+
+    $scope.add = function() {
+        var concert = {
+            name: $scope.newName,
+            location: $scope.newLocation,
+            time: $scope.newTime,
+            owner: $scope.userid
+        };
+        console.log("this is concert" + JSON.stringify(profile));
+        concertsServices.add(concert).then(function(res) {
+            console.log('your concert has been added.:' + JSON.stringify(res.data));
+        });
+    };
+});
