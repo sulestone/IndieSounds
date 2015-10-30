135c32a92d3fb5ceec4636c57823c17821f5d194
Sorum Panchal <svpanchal@gmail.com>
2015-10-30 10:31:52 -0400

    Created profiles and profile page

--- client/app/profiles/profiles.js
+++ client/app/profiles/profiles.js
@@ -3,24 +3,28 @@
 angular.module('indiesoundsApp')
   .config(function ($stateProvider) {
     $stateProvider
+      //Allow a user to add information to their profile
       .state({
         name: 'newprofile',
         url: '/newprofile',
         templateUrl: 'app/profiles/newprofile.html',
         controller: 'NewProfileCtrl'
       })
+      //profiles listing for all IndieSounds users
       .state({
         name: 'profiles',
         url: '/profiles',
         templateUrl: 'app/profiles/profiles.html',
         controller: 'ProfilesCtrl'
       })
+      //show individual user profile
       .state({
         name: 'profile',
         url: '/profiles/:profileId',
         templateUrl: 'app/profiles/profile.html',
         controller: 'ProfileCtrl'
       })
+      //show a user's profile settings
       .state({
         name: 'profilesettings',
         url: '/profiles/edit/:profileId',
