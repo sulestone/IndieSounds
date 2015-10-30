5d00c8b3718b2c63f81d38d94880e1f25194ea31
Sule Stone <sulestone@att.net>
2015-10-30 14:37:17 -0400

    updated navbar

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,37 +1,39 @@
 'use strict';
 
 angular.module('indiesoundsApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': 'main'
-    },
-    {
-      'title': 'Listen to music',
-      'link': 'soundcloud'
-    }];
+    .controller('NavbarCtrl', function($scope, $location, Auth) {
+        $scope.menu = [{
+            'title': 'Home',
+            'link': 'main'
+        }, {
+            'title': 'Listen to music',
+            'link': 'soundcloud'
+        }];
 
-    Auth.getCurrentUser().$promise.then(function(currentUser) {
-  		$scope.profile = currentUser.myProfile;
-  		console.log('$scope.userid is: ' + JSON.stringify($scope.userid));
-  	});
-
-    $scope.isCollapsed = true;
-    $scope.isLoggedIn = Auth.isLoggedIn;
-    $scope.isAdmin = Auth.isAdmin;
-    $scope.getCurrentUser = Auth.getCurrentUser;
-
-    $scope.logout = function() {
-      Auth.logout();
-      $location.path('/login');
-    };
-    $scope.profileSettings = function() {
-      $location.path('/profiles/edit/' + $scope.profile);
-      console.log('This is current user: ' + $scope.userid);
-    };
+        if (Auth.isLoggedIn()) {
+            Auth.getCurrentUser().$promise.then(function(currentUser) {
+                $scope.profile = currentUser.myProfile;
+                console.log('$scope.userid is: ' + JSON.stringify($scope.userid));
+            });
+        };
 
 
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
-  });
+        $scope.isCollapsed = true;
+        $scope.isLoggedIn = Auth.isLoggedIn;
+        $scope.isAdmin = Auth.isAdmin;
+        $scope.getCurrentUser = Auth.getCurrentUser;
+
+        $scope.logout = function() {
+            Auth.logout();
+            $location.path('/login');
+        };
+        $scope.profileSettings = function() {
+            $location.path('/profiles/edit/' + $scope.profile);
+            console.log('This is current user: ' + $scope.userid);
+        };
+
+
+        $scope.isActive = function(route) {
+            return route === $location.path();
+        };
+    });
